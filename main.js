90b23c63af0dc1820a78995bda7be7594cdea5a8
Some One <1234abcdcba4321@users.noreply.github.com>
2017-12-30 15:55:22 -0600

    saving

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ function ButtonClick()
   document.getElementById("money").innerHTML="You have " + player.buttons + " buttons."
 }
 var player = {buttons:0}
+
+function set_save(name, value) {
+    localStorage.setItem(name, btoa(JSON.stringify(value, function(k, v) { return (v === Infinity) ? "Infinity" : v; })))
+}
+
+function get_save(name) {
+    if (localStorage.getItem(name) !== null) {
+        return JSON.parse(atob(localStorage.getItem(name), function(k, v) { return (v === Infinity) ? "Infinity" : v; }))
+    }
+}
+
+//autosave
+setInterval(function () {
+    set_save("autosave",player)
+}, 30000);
+
+//load game
+
+var save_data = get_save('autosave');
+if (save_data) {
+  player = save_data;
+}
